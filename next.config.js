cb2b6c9cc96728d73923420955c3db69151c411f
Luckyhv <harshavardhan9680@gmail.com>
2024-03-17 22:15:52 +0530

    V3.0.1

--- next.config.js
+++ next.config.js
@@ -18,19 +18,19 @@ const nextConfig = {
         domains: ['s4.anilist.co','artworks.thetvdb.com','media.kitsu.io', 'image.tmdb.org'],
         unoptimized: true
       },
-      async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    },
+    //   async headers() {
+    //     return [
+    //         {
+    //             source: "/api/:path*",
+    //             headers: [
+    //                 { key: "Access-Control-Allow-Credentials", value: "true" },
+    //                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+    //                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    //                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    //             ]
+    //         }
+    //     ]
+    // },
     typescript: {
       // !! WARN !!
       // Dangerously allow production builds to successfully complete even if
