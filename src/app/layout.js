803561929b366ff1b4e6842c576f4c3cc24714fa
Sp9751 <s96112516@gmail.com>
2024-07-13 16:17:00 +0530

    Add env file

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,9 +16,9 @@ import { AuthProvider } from './SessionProvider';
 const inter = Inter({ subsets: ['latin'] })
 //const myfont = localFont({ src: "../static-fonts/AldotheApache.ttf" })
 
-const APP_NAME = "Airin";
-const APP_DEFAULT_TITLE = "Airin - Watch Anime Online";
-const APP_DESCRIPTION = "Discover a world of anime without interruptions on Airin! Watch your favorite series for free, with no ads to disrupt your viewing experience. Join now and immerse yourself in the captivating stories and vibrant characters that Airin has to offer!";
+const APP_NAME = "Free Guy Watch";
+const APP_DEFAULT_TITLE = "FGW - Watch Anime Online";
+const APP_DESCRIPTION = "Discover a world of anime without interruptions on Free Guy Watch! Watch your favorite series for free, with no ads to disrupt your viewing experience. Join now and immerse yourself in the captivating stories and vibrant characters that Airin has to offer!";
 
 export const metadata = {
   metadataBase: new URL('https://aniplaynow.live'),
